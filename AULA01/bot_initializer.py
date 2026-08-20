e6bfd70393e6ddfdb30af5e1d4c60bcce4c2a08a
Shy <kimuralivia@gmail.com>
2026-08-20 20:14:22 -0300

    RESPOSTA

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,16 @@
+BOT_NAME: str = "PANDASSHINY"
+MAX_RETRIES: int = 7
+EXECUTION_TIMEOUT: float = 120.0
+IS_PRODUCTION: bool = True
+
+print("===bot initialization===")
+
+print("BOT_NAME: ", BOT_NAME, " | typer: ")
+print(type(BOT_NAME))
+print("BOT_NAME: ", MAX_RETRIES, " | typer: ")
+print(type(MAX_RETRIES))
+print("BOT_NAME: ", EXECUTION_TIMEOUT, " | typer: ")
+print(type(EXECUTION_TIMEOUT))
+print("BOT_NAME: ", IS_PRODUCTION, " | typer: ")
+print(type(IS_PRODUCTION))
+
